62cd7fc0bddad14379eb7e203e1ae3bf84b9d888
modalsoul <modalsoul0215@gmail.com>
2014-02-04 00:50:58 +0900

    add clearing rows process

--- library/src/main/scala/Stage.scala
+++ library/src/main/scala/Stage.scala
@@ -1,6 +1,9 @@
 package jp.modal.soul.tetrix
 
+
 object Stage {
+	import scala.annotation.tailrec
+
 	def newState(blocks:Seq[Block]):GameState = {
 		val size = (10, 20)
 		val dummy = Piece((0,0), TKind)
@@ -9,9 +12,24 @@ object Stage {
 	val moveLeft = transit { _.moveBy(-1.0, 0.0)}
 	val moveRight = transit {_.moveBy(1.0, 0.0)}
 	val rotateCW= transit {_.rotateBy(-math.Pi/2.0)}
-	val tick = transit (_.moveBy(0.0, -1.0), spawn)
+	val tick = transit (_.moveBy(0.0, -1.0), 
+				Function.chain(clearFullRow :: spawn :: Nil))
 	
-	private[this] def spawn(s:GameState):GameState = {
+	private[this] lazy val clearFullRow: GameState => GameState =
+		(s0:GameState) => {
+			def isFullRow(i:Int, s:GameState):Boolean =
+				(s.blocks filter {_.pos._2 == i} size) == s.gridSize._1
+			@tailrec def tryRow(i:Int, s:GameState):GameState =
+				if(i<0) s
+				else if(isFullRow(i,s)) 
+					tryRow(i-1, s.copy(blocks = (s.blocks filter {_.pos._2 < i}) ++
+						(s.blocks filter {_.pos._2 > i} map { b =>
+							b.copy(pos = (b.pos._1, b.pos._2 -1)) })))
+				else tryRow(i -1, s)
+			tryRow(s0.gridSize._2 -1, s0)
+		}
+
+	private[this] lazy val  spawn:GameState => GameState = (s:GameState) => {
 		def dropOffPos = (s.gridSize._1/2.0, s.gridSize._2 -3.0)
 		val p = Piece(dropOffPos, TKind)
 		s.copy(blocks = s.blocks ++ p.current, currentPiece = p)
